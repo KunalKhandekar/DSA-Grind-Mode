eb2d8d04bf9d3294d8c0bcc89441cc0d7c3f45b5
KunalKhandekar <kunalkhandekar010@gmail.com>
2025-11-14 10:37:12 +0530

    3 LinkedList Problems

--- LinkedList/8-RemoveNthNodeFromEnd.js
+++ LinkedList/8-RemoveNthNodeFromEnd.js
@@ -1,25 +1,43 @@
-// Leetcode 18 -> Remove Nth node from end
+// Leetcode 19 -> Remove Nth node from end
 
+// Approach 1
+const removeNthFromEnd1 = function (head, n) {
+  const sentinelNode = new ListNode();
+  sentinelNode.next = head;
 
-const removeNthFromEnd = function (head, n) {
-    const sentinelNode = new ListNode();
-    sentinelNode.next = head;
+  let length = 0;
+  while (head) {
+    head = head.next;
+    length++;
+  }
 
-    let length = 0;
-    while (head) {
-        head = head.next;
-        length++;
-    }
+  let prevNode = length - n;
 
-    let prevNode = length - n;
+  let prev = sentinelNode;
+  for (let i = 0; i < prevNode; i++) {
+    prev = prev.next;
+  }
+  prev.next = prev.next.next;
 
-    let prev = sentinelNode;
-    for(let i = 0; i < prevNode; i++) {
-        prev = prev.next;
-    }
-    prev.next = prev.next.next;
-
-    return sentinelNode.next;
+  return sentinelNode.next;
 };
 
+// Approach 2
+const removeNthFromEnd2 = function (head, n) {
+  const sentinelNode = new ListNode();
+  sentinelNode.next = head;
 
+  let start = sentinelNode;
+  let length = 0;
+  let first = sentinelNode;
+  while (first.next) {
+    if (length >= n) {
+      start = start.next;
+    }
+    first = first.next;
+    length++;
+  }
+  start.next = start.next.next;
+
+  return sentinelNode.next;
+};
---
+++ LinkedList/9-RemoveDuplicatesFromSortedList.js
@@ -0,0 +1,15 @@
+//  Leetcode 83 -> Remove Duplicates from sorted list
+
+function deleteDuplicates(head) {
+  let curr = head;
+
+  while (curr && curr.next) {
+    if (curr.val === curr.next.val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+
+  return head;
+}
