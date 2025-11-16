0a1cc05c1a5724cb94ad8c3f8e2eb1202eae0f5c
KunalKhandekar <kunalkhandekar010@gmail.com>
2025-11-16 09:58:12 +0530

    Merge Sorted List

---
+++ LinkedList/12-MergeSortedLists.js
@@ -0,0 +1,27 @@
+// Leectcode 21 -> Merge Sorted list
+
+function mergeTwoLists(l1, l2) {
+    let start = new ListNode();
+    let curr = start;
+
+    while(l1 && l2) {
+        if(l1.val < l2.val) {
+            curr.next = l1;
+            l1 = l1.next;
+        } else {
+            curr.next = l2;
+            l2 = l2.next;
+        }
+        curr = curr.next;
+    }
+
+    if(l1) {
+        curr.next = l1;
+    }
+
+    if(l2) {
+        curr.next = l2;
+    }
+
+    return start.next;
+}
