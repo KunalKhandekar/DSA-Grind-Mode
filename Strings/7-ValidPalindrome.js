464f4b3fa9c28c92b4a73b012a4c7ecde947a7a2
KunalKhandekar <kunalkhandekar010@gmail.com>
2025-11-25 11:07:12 +0530

    1 string problem

---
+++ Strings/7-ValidPalindrome.js
@@ -0,0 +1,32 @@
+// Approach 1
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  let filter = "";
+  let rev = "";
+  s = s.toLowerCase();
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i].match(/[a-z0-9]/i)) {
+      filter += s[i];
+      rev = s[i] + rev;
+    }
+  }
+
+  return filter === rev;
+};
+
+// Approach 2
+/**
+ * @param {string} s
+ * @return {boolean}
+ * //
+ */
+
+ 
+
+
+console.log(isPalindrome('A man, a plan, a canal: Panama'))
