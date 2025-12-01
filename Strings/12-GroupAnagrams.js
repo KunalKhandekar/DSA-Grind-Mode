6df071ad61c5eae5a1e037d0c567573290b8ff86
KunalKhandekar <kunalkhandekar010@gmail.com>
2025-12-01 09:27:22 +0530

    group anagram approach 2

--- Strings/12-GroupAnagrams.js
+++ Strings/12-GroupAnagrams.js
@@ -10,8 +10,31 @@ const groupAnagrams = (strs) => {
     // } else {
     //   map[key].push(strs[i]);
     // }
-    (map[key] = map[key] || []).push(strs[i])
+    (map[key] = map[key] || []).push(strs[i]);
   }
 
   return [...Object.values(map)];
 };
+
+const groupAnagramsApproach2 = (strs) => {
+  let map = {};
+
+  for (let i = 0; i < strs.length; i++) {
+    // Create a key
+    let freq = Array(26).fill(0);
+    let s = strs[i];
+    for (let j = 0; j < s.length; j++) {
+      let idx = s[j].charCodeAt() - "a".charCodeAt();
+      ++freq[idx];
+    }
+    let key = "";
+    for (let i = 0; i < freq.length; i++) {
+      key += `#${freq[i]}`;
+    }
+
+    (map[key] = map[key] || []).push(strs[i]);
+  }
+
+  return [...Object.values(map)];
+};
+
